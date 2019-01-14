c2e6399268c411916853ea53704d2d2360f392c2
Alexander Zulechner <alexanderzulechner@gmail.com>
2019-01-14 09:52:03 +0100

    chore(package.json): updates the jest versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ const SLIDER_PROPS: IProps = {
 };
 
 @Component({
-  selector: "app-root",
+  selector: "app-component",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
